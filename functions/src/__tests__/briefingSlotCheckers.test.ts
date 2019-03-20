55022b14d79e8557f260bc9f6b5ed2a98a610a04
Susie Coleman <susie.coleman@guardian.co.uk>
2019-03-20 16:33:17 +0000

    Update briefing change over times.

--- functions/src/__tests__/briefingSlotCheckers.test.ts
+++ functions/src/__tests__/briefingSlotCheckers.test.ts
@@ -70,37 +70,78 @@ describe('isWeekdayAM', () => {
 });
 
 describe('isSaturday', () => {
-  test('should return true on Saturday', () => {
-    const time = moment().day('saturday');
+  test('should return true on Saturday from 6am', () => {
+    const time = moment()
+      .day('saturday')
+      .hour(6);
 
     expect(isSaturday(time)).toEqual(true);
+  });
+  test('should return false on Saturday before 6am', () => {
+    const time = moment()
+      .day('saturday')
+      .hour(5)
+      .minute(59);
+
+    expect(isSaturday(time)).toEqual(false);
   });
   test('should return false on Friday', () => {
     const time = moment().day('friday');
 
     expect(isSaturday(time)).toEqual(false);
   });
-  test('should return false on Sunday', () => {
-    const time = moment().day('sunday');
+  test('should return true on Sunday before 6am', () => {
+    const time = moment()
+      .day('sunday')
+      .hour(5)
+      .minute(59);
+
+    expect(isSaturday(time)).toEqual(true);
+  });
+  test('should return false on Sunday after 5:59am', () => {
+    const time = moment()
+      .day('sunday')
+      .hour(6);
 
     expect(isSaturday(time)).toEqual(false);
   });
 });
 
 describe('isSunday', () => {
-  test('should return true on Sunday', () => {
-    const time = moment().day('sunday');
+  test('should return true on Sunday from 6am', () => {
+    const time = moment()
+      .day('sunday')
+      .hour(6);
 
     expect(isSunday(time)).toEqual(true);
   });
+  test('should return false on Sunday before 6am', () => {
+    const time = moment()
+      .day('sunday')
+      .hour(5)
+      .minute(59);
+
+    expect(isSunday(time)).toEqual(false);
+  });
+
   test('should return false on Saturday', () => {
     const time = moment().day('saturday');
 
     expect(isSunday(time)).toEqual(false);
   });
-  test('should return false on Monday', () => {
-    const time = moment().day('monday');
+  test('should return false on Monday after 5:59am', () => {
+    const time = moment()
+      .day('monday')
+      .hour(6);
 
     expect(isSunday(time)).toEqual(false);
+  });
+  test('should return true on Monday before 6am', () => {
+    const time = moment()
+      .day('monday')
+      .hour(5)
+      .minute(59);
+
+    expect(isSunday(time)).toEqual(true);
   });
 });
